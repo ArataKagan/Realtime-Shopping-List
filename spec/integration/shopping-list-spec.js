b9a0b758ebe35e4c8cc92b959a5b638e0561a18c
Arata Kagan <aratakagan@gmail.com>
2020-09-08 23:50:39 -0700

    Fail Post request for adding item

--- spec/integration/shopping-list-spec.js
+++ spec/integration/shopping-list-spec.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:5000/api/shopping-list";
+const base = "http://localhost:5000/api/shopping-list/";
 const sequelize = require("../../src/db/models/index").sequelize;
 const Item = require("../../src/db/models").Item;
 
@@ -32,6 +32,33 @@ describe("routes : shopping-list", () => {
                 expect(body).toContain("apple");
                 done();
             });
+        });
+    });
+
+    describe("POST /shopping-list/create", () => {
+        const options = {
+            url: `${base}create`,
+            form: {
+                item: "orange",
+                quantity: 2
+            }
+        };
+
+        it("should create a new item", (done) => {
+            request.post(options,
+                (err, res, body) => {
+                    Item.findOne({ where: { item: "orange" } })
+                        .then((item) => {
+                            expect(res.statusCode).toBe(303);
+                            expect(item.item).toBe("orange");
+                            expect(item.quantity).toBe(2)
+                            done();
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            done();
+                        });
+                });
         });
     });
 
