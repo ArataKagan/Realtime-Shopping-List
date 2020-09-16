cdf14f3832d13d64ff4dadca3613c50b34a23fcf
Arata Kagan <aratakagan@gmail.com>
2020-09-15 23:55:08 -0700

    Add shopping list component and dummy text

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import ShoppingList from './components/ShoppingList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <p>Welcome to Shopping List App</p>
+        <ul>
+          <ShoppingList />
+          <ShoppingList />
+        </ul>
       </header>
     </div>
   );
---
+++ client/src/components/ShoppingList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ShoppingList extends Component {
+  render() {
+    return <li>a shopping list goes here</li>;
+  }
+}
+
+export default ShoppingList;
